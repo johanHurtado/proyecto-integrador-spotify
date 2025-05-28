a07bfe9a3acaa11a7fe099538ab66e3910eb79f8
EstebanC <cwilliamesteban@gmail.com>
2025-05-27 22:57:27 -0500

    Listo el dao de Song

---
+++ Spotify/src/entities/SongDAO.java
@@ -0,0 +1,117 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import Database.Conexion;
+
+public class SongDAO {
+    // ! CRUD
+
+    // Metodo para obtener todas las canciones
+    public List<Song> getAllSongs() {
+        List<Song> songs = new ArrayList<>();
+        String sql = "SELECT * FROM canciones";
+        try {
+            Connection conn = Conexion.getConnection();
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+
+            while (rs.next()) {
+                Song song = new Song(rs.getInt("idcancion"), rs.getString("titulo"), rs.getString("descripcion"),
+                        rs.getDouble("duracion"),
+                        new Artist(rs.getInt("idartista"), rs.getString("nombre_artista"), ""),
+                        new Gender(rs.getInt("idgenero"), rs.getString("nombre_genero"), ""));
+                songs.add(song);
+            }
+
+        } catch (Exception e) {
+            System.out.println("No se pudo obtener las canciones: " + e.getMessage());
+        }
+        return songs;
+    }
+
+    // Metodo para agregar una canción
+    public boolean addSong(Song song) {
+        String sql = "INSERT INTO canciones (titulo, descripcion, duracion, idartista, idgenero) VALUES (?, ?, ?, ?, ?)";
+
+        try {
+            Connection conn = Conexion.getConnection();
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, song.getTitle());
+            ps.setString(2, song.getDescription());
+            ps.setDouble(3, song.getTimeLength());
+            ps.setInt(4, song.getArtist().getId());
+            ps.setInt(5, song.getGender().getIdGender());
+            ps.executeUpdate();
+            return true;
+
+        } catch (Exception e) {
+            System.out.println("No se pudo agregar la canción: " + e.getMessage());
+            return false;
+        }
+    }
+
+    // Metodo para actualizar una canción
+    public boolean updateSong(Song song) {
+        String sql = "UPDATE canciones SET titulo = ?, descripcion = ?, duracion = ?, idartista = ?, idgenero = ? WHERE idcancion = ?";
+
+        try {
+            Connection conn = Conexion.getConnection();
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, song.getTitle());
+            ps.setString(2, song.getDescription());
+            ps.setDouble(3, song.getTimeLength());
+            ps.setInt(4, song.getArtist().getId());
+            ps.setInt(5, song.getGender().getIdGender());
+            ps.setInt(6, song.getIdSong());
+            ps.executeUpdate();
+            return true;
+
+        } catch (Exception e) {
+            System.out.println("No se pudo actualizar la canción: " + e.getMessage());
+            return false;
+        }
+    }
+
+    // Metodo para eliminar una canción por ID
+    public boolean deleteSongById(int idSong) {
+        String sql = "DELETE FROM canciones WHERE idcancion = ?";
+
+        try {
+            Connection conn = Conexion.getConnection();
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setInt(1, idSong);
+            ps.executeUpdate();
+            return true;
+
+        } catch (Exception e) {
+            System.out.println("No se pudo eliminar la canción: " + e.getMessage());
+            return false;
+        }
+    }
+
+    // Metodo para obtener una canción por ID
+    public Song getSongById(int idSong) {
+        Song song = null;
+        String sql = "SELECT * FROM canciones WHERE idcancion = ?";
+
+        try {
+            Connection conn = Conexion.getConnection();
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setInt(1, idSong);
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                song = new Song(rs.getInt("id_cancion"), rs.getString("titulo"),
+                        rs.getString("descripcion"), rs.getDouble("duracion"),
+                        new Artist(rs.getInt("id_artista"), rs.getString("nombre_artista"), ""),
+                        new Gender(rs.getInt("id_genero"), rs.getString("nombre_genero"), ""));
+            }
+        } catch (Exception e) {
+            System.out.println("No se pudo obtener la canción: " + e.getMessage());
+        }
+        return song;
+    }
+}
