92ec0eb7a9b3901535f76afe4471d9f19b2ea64b
JuanJ <juanitorinconm.angeles@gmail.com>
2025-05-27 19:02:38 -0500

    DAO GenderDAO

--- Spotify/src/infrastructure/GenderDAO.java
+++
@@ -1,9 +0,0 @@
-package infrastructure;
-import java.util.List;
-
-public interface GenderDAO {
-    void create(GenderDAO gender);
-    void update(GenderDAO gender);
-    void delete(GenderDAO gender);
-    List<GenderDAO>consultAll();
-}
